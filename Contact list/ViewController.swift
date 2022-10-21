0dc223f455bdf41ae1b2ccef571d2f4755d0526a
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-10-21 21:23:01 +0300

    Initial Commit

---
+++ Contact list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact list
+//
+//  Created by Александр on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
